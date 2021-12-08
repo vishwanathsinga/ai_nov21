e9eed198973d72ec7e0604c60abc13dd2aa2e4fc
vishwanath <vishwanathsinga@gmail.com>
2021-12-08 10:23:25 +0100

    Update ravi.py

--- Chapter 1/7. Web-scraping/ravi.py
+++ Chapter 1/7. Web-scraping/ravi.py
@@ -1,2 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+
+page = requests.get("https://weather.com/weather/tenday/l/San+Francisco+CA?canonicalCityId=dfdaba8cbe3a4d12a8796e1f7b1ccc7174b4b0a2d5ddb1c8566ae9f154fa638c")
+soup = BeautifulSoup(page.content,'html.parser')
+soup.prettify()
+
+
+
+list =soup.find("div" ,class_="LocationPageTitle")
+
+print(list)
+
+
